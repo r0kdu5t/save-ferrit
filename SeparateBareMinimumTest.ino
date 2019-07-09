483320996f3a353cfc8e2f84f2b2d5b67329bd7f
Andrew Sands <andrew@theatrix.org.nz>
2019-07-09 14:40:12 +1200

    And some more worth following up on.

--- SeparateBareMinimumTest.ino
+++ SeparateBareMinimumTest.ino
@@ -1,25 +1,25 @@
 /**
- * Program definition sentence - objectives
- * 
- * ADD LICENSE here ==>
- * 
- */
+   Program definition sentence - objectives
+
+   ADD LICENSE here ==>
+
+*/
 
 /*
- * INCLUDES
- */
-#define DEBUG
-#include <DebugUtils.h> 
+   INCLUDES
+*/
+//#define DEBUG
+#include <DebugUtils.h>
 #include "CONFIG.h"
 
 /*
- * CONFIGURATION bits
- */
+   CONFIGURATION bits
+*/
 
 // constants won't change. Used here to set a pin number:
 const int ledPIN = 3;  // the number of the LED pin
 const long interval = 500;           // interval at which to blink (milliseconds)
- 
+
 // Values defined in CONFIG.h
 const char secret[] = SECRET;
 const char hostname[] = HOSTNAME;
@@ -37,28 +37,34 @@ unsigned long previousMillis = 0;        // will store last time LED was updated
 
 int pushButton = 2;       // As described on pin D2
 /*
- * SETUP - // put your setup code here, to run once:
- */
+   SETUP - // put your setup code here, to run once:
+*/
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+#ifdef DEBUG
+  Serial.println("DEBUG set");
+#else
+  Serial.println("DEBUG unset");
+#endif
+
   // make the pushbutton's pin an input:
   pinMode(pushButton, INPUT);
   // set the digital pin as output:
   pinMode(ledPIN, OUTPUT);
-    digitalWrite(ledPIN, HIGH);  
+  digitalWrite(ledPIN, HIGH);
 
-// ======================================================================
-   DEBUG_PRINT("interval");
-   DEBUG_PRINT(interval);
+  // ======================================================================
+  DEBUG_PRINT("interval");
+  DEBUG_PRINT(interval);
 
-   DEBUG_PRINT("ledPIN");
-   DEBUG_PRINT(ledPIN);
+  DEBUG_PRINT("ledPIN");
+  DEBUG_PRINT(ledPIN);
 }
 
 /*
- * LOOP - // put your main code here, to run repeatedly:
- */
+   LOOP - // put your main code here, to run repeatedly:
+*/
 void loop() {
   // read the input pin:
   int buttonState = digitalRead(pushButton);
